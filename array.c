b9a0ac7ab878a815e3fde509f11bae13ec89ef55
tvieregge <tmvieregge@gmail.com>
2019-05-28 12:43:18 -0400

    expanding array example

--- array.c
+++ array.c
@@ -3,8 +3,19 @@
 
 int main(void) {
     int a[10];
+
+    // with array syntax
     for(int i=0; i<10; i++) {
         a[i] = (i+1)*(i+1);
+    }
+
+    for(int i=0; i<10; i++) {
+        printf("%d\n", a[i]);
+    }
+
+    // with pointer syntax
+    for(int i=0; i<10; i++) {
+        *(a+i) = i;
     }
 
     for(int i=0; i<10; i++) {
