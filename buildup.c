0cdb0e0200a386ef3fe87d659d2809ad5a85f1b6
tvieregge <tmvieregge@gmail.com>
2019-05-28 12:29:34 -0400

    changed to array example

---
+++ buildup.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void) {
+    int a[10];
+    for(int i=0; i<10; i++) {
+        a[i] = (i+1)*(i+1);
+    }
+
+    for(int i=0; i<10; i++) {
+        printf("%d\n", a[i]);
+    }
+
+    return EXIT_SUCCESS;
+}
